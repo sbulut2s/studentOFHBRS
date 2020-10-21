ba8f2be04d1c0202fbed6989cb1faee2b82e145a
saschaalda <sascha.alda@h-brs.de>
2020-10-21 18:50:19 +0200

    Erster Checkin der Klassen und des Interfaces

---
+++ src/org/hbrs/se/ws20/uebung1/control/GermanTranslator.java
@@ -0,0 +1,19 @@
+package org.hbrs.se.ws20.uebung1.control;
+
+public class GermanTranslator implements Translator {
+	
+
+	public String translateNumber( int number ) { 
+		// [ihr Source Code aus Übung 1-2] 
+		return null;
+	}
+		
+	/*
+	 * Objektmethode der Klasse GermanTranslator
+	 * zur Ausgabe einer Info. 
+	 */
+	public void printInfo() {
+		System.out.println("GermanTranslator c / o 2020 by HBRS");
+
+	}
+}
---
+++ src/org/hbrs/se/ws20/uebung1/view/Client.java
@@ -0,0 +1,22 @@
+package org.hbrs.se.ws20.uebung1.view;
+
+class Client {
+	
+
+	/*
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 */
+	public void display( int aNumber ){
+		// In dieser Methode soll die Methode translateNumber 
+		// mit dem übergegebenen Wert der Variable aNumber 
+		// aufgerufen werden
+
+		System.out.println("Das Ergebnis der Berechnung: " +
+				"[das Ergebnis an dieser Stelle]" );
+
+	}
+}
+
+
+
+
