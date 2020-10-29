6cee32d116361d7fd8e2472650a6bb3868f3143d
saschaalda <sascha.alda@h-brs.de>
2020-10-29 13:16:35 +0100

    Mein erster Test

---
+++ test/org/hbrs/se/ws20/uebung1/view/test/ClientTest.java
@@ -0,0 +1,10 @@
+package org.hbrs.se.ws20.uebung1.view.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @org.junit.jupiter.api.Test
+    void display() {
+    }
+}
